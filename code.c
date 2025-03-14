1bbf85ca5f2da07a7cfd0341ab8dee4de734d9c2
ignackocom <peter.potrok@outlook.sk>
2025-03-14 09:51:01 +0100

    Add files via upload

---
+++ code.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+* @file      header.h
+* @author    author
+* @copyright copyright yyyy-yyyy
+*            xyz License (see License.txt file)
+* @brief     description
+* @details   full description
+*
+* @see       website, file, etc.
+******************************************************************************/
+
+#include "header.h"
---
+++ header.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+* @file      header.h
+* @author    author
+* @copyright copyright yyyy-yyyy
+*            xyz License (see License.txt file)
+* @brief     description
+* @details   full description
+*
+* @see       website, file, etc.
+******************************************************************************/
+
+#ifndef HEADER_H
+#define HEADER_H	000000L
+
+
+/******************************************************************************
+* @brief
+*	item description
+* 
+* @param 
+*	param1 - description
+* @param
+*	paramn - description
+*
+* @return
+*	description
+*
+* @note
+*
+* @code{.language extension}
+* ...
+* code example
+* ...
+* @endcode
+*
+*/
+
+
+#endif /* HEADER_H */
