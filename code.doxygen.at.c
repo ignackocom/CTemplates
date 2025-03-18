c7948d6fc580294ae60fee84e4d05cb3daca9c91
ignackocom <peter.potrok@outlook.sk>
2025-03-18 10:08:03 +0100

    Add files via upload

---
+++ code.doxygen.at.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+* @file      Code.c
+* @version   YYYY.MM.DD.RR (YYYY year,MM month,DD day,RR revision)
+* @author    Author
+* @copyright Copyright (c) yyyy - yyyy
+*            XYZ License (see License.txt file)
+* @brief     Description
+* @details   Full description
+*
+* @see       Website, file, etc.
+******************************************************************************/
+
+#include "header.doxygen.at.h"
